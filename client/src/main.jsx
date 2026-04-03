34e4a9b4f120f5fa13fe8eb582628d67d6ce9c86
Jhame Letigio <jamletigio@gmail.com>
2026-04-03 20:05:17 +0800

    add keyframes blur

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"
 import './index.css'
 import App from './pages/App.jsx'
 import Callback from './pages/Callback.jsx'
 import TopTracks from './pages/TopTracks.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <div key={location.pathname} className="route-enter-blur">
+      <Routes location={location}>
         <Route path="/" element={<App />} />
         <Route path="/callback" element={<Callback />} />
         <Route path="/top-tracks" element={<TopTracks />} />
       </Routes>
+    </div>
+  );
+}
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <AnimatedRoutes />
     </BrowserRouter>
   </StrictMode>,
 )
