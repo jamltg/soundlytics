caa42e54bc5ef08e0cb46cb34e21c8ada62f6f5a
Jhame Letigio <jamletigio@gmail.com>
2026-04-03 19:14:13 +0800

    add top tracks short term

--- client/src/pages/TopTracks.jsx
+++ client/src/pages/TopTracks.jsx
@@ -39,7 +39,7 @@ export default function TopTracks() {
         }
 
         const tracksRes = await fetch(
-          "https://api.spotify.com/v1/me/top/tracks?limit=5&time_range=long_term",
+          "https://api.spotify.com/v1/me/top/tracks?limit=5&time_range=short_term",
           {
             headers: { Authorization: `Bearer ${accessToken}` },
           }
