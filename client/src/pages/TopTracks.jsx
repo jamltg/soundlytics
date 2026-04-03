356f97dc74f5c275eab1855ae06b2b9d6100f8d5
Jhame Letigio <jamletigio@gmail.com>
2026-04-03 19:39:43 +0800

    Change fetch for top tracks

--- client/src/pages/TopTracks.jsx
+++ client/src/pages/TopTracks.jsx
@@ -39,7 +39,7 @@ export default function TopTracks() {
         }
 
         const tracksRes = await fetch(
-          "https://api.spotify.com/v1/me/top/tracks?limit=5&time_range=short_term",
+          "https://api.spotify.com/v1/me/player/recently-played?limit=5",
           {
             headers: { Authorization: `Bearer ${accessToken}` },
           }
